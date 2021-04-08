61dee10cfd84194a3b24e64c5375b07324eb88b8
alfiana00 <alfiananurazizah75@gmail.com>
2021-04-08 11:37:46 +0700

    update tentang

--- app/src/main/java/com/example/aplikasiku/activity/VolumeChart.java
+++ app/src/main/java/com/example/aplikasiku/activity/VolumeChart.java
@@ -218,7 +218,7 @@ public class VolumeChart extends AppCompatActivity {
         lineDataSetA.setCircleRadius(4f);
         lineDataSetA.setDrawCircleHole(false);
         lineDataSetA.setValueTextSize(0f);
-        lineDataSetA.setDrawFilled(true);
+        lineDataSetA.setDrawFilled(false);
         lineDataSetA.setFormLineWidth(1f);
         lineDataSetA.setMode(LineDataSet.Mode.HORIZONTAL_BEZIER);
         lineDataSetA.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
@@ -233,7 +233,7 @@ public class VolumeChart extends AppCompatActivity {
         lineDataSetB.setCircleRadius(4f);
         lineDataSetB.setDrawCircleHole(false);
         lineDataSetB.setValueTextSize(0f);
-        lineDataSetB.setDrawFilled(true);
+        lineDataSetB.setDrawFilled(false);
         lineDataSetB.setFormLineWidth(1f);
         lineDataSetB.setMode(LineDataSet.Mode.LINEAR);
         lineDataSetB.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
@@ -248,7 +248,7 @@ public class VolumeChart extends AppCompatActivity {
         lineDataSetC.setCircleRadius(4f);
         lineDataSetC.setDrawCircleHole(false);
         lineDataSetC.setValueTextSize(0f);
-        lineDataSetC.setDrawFilled(true);
+        lineDataSetC.setDrawFilled(false);
         lineDataSetC.setFormLineWidth(1f);
         lineDataSetC.setMode(LineDataSet.Mode.LINEAR);
         lineDataSetC.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
@@ -263,7 +263,7 @@ public class VolumeChart extends AppCompatActivity {
         lineDataSetD.setCircleRadius(4f);
         lineDataSetD.setDrawCircleHole(false);
         lineDataSetD.setValueTextSize(0f);
-        lineDataSetD.setDrawFilled(true);
+        lineDataSetD.setDrawFilled(false);
         lineDataSetD.setFormLineWidth(1f);
         lineDataSetD.setMode(LineDataSet.Mode.LINEAR);
         lineDataSetD.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
@@ -278,7 +278,7 @@ public class VolumeChart extends AppCompatActivity {
         lineDataSetP.setCircleRadius(4f);
         lineDataSetP.setDrawCircleHole(false);
         lineDataSetP.setValueTextSize(0f);
-        lineDataSetP.setDrawFilled(true);
+        lineDataSetP.setDrawFilled(false);
         lineDataSetP.setFormLineWidth(1f);
         lineDataSetP.setMode(LineDataSet.Mode.LINEAR);
         lineDataSetP.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
